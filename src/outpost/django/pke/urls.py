522551effdb7bd44229396d25c8ea944a31a1b4a
Michael Fladischer <michael@fladi.at>
2023-08-01 12:36:37 +0200

    Set urlconf app_name.

--- src/outpost/django/pke/urls.py
+++ src/outpost/django/pke/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = "pke"
+
 urlpatterns = [
     url(r"^xml$", views.XMLView.as_view(), name="xml"),
     url(r"^schema$", views.SchemaView.as_view(), name="schema"),
